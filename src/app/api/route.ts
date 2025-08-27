c24326f42f1d9587c1a464d3941131a950e20867
Rick Perez <rperezkc35@gmail.com>
2025-08-27 15:08:43 -0500

    logging in get route for debugging

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres";
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(req: Request) {
-  
+  // 
   try{
     const body = await req.json()
     const {
